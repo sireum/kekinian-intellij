a145dabd939dd7f93f016fcb7f199ffee5acfdce
Robby <robby@k-state.edu>
2020-06-19 21:38:16 -0500

    Updated versioning.

--- post.sh
+++ post.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 7z d sireum-kekinian-intellij.zip sireum-kekinian-intellij/lib/sireum.jar
-VER=$(git log -n 1 --date=format:%Y%m%d.%H%M --pretty=format:4.%cd.%h)
+VER=$(git log -n 1 --date=format:%Y%m%d --pretty=format:4.%cd.%h)
 7z x sireum-kekinian-intellij.zip sireum-kekinian-intellij/lib/sireum-kekinian-intellij.jar
 7z x sireum-kekinian-intellij/lib/sireum-kekinian-intellij.jar META-INF/plugin.xml
 sed -i.bak "s/4.0.0-SNAPSHOT/${VER}/g" META-INF/plugin.xml
